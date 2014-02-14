805359c6a8c80d47dda6a6f3f745f8f15ee77a21
j3ck <fox_raiodin@mail.ru>
2014-02-14 23:35:29 +0300

    home page configurate for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 MaketStorage::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registration]
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lay out with "rake routes".\
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root "devise/sessions#new"
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
