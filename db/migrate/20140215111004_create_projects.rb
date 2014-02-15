44da4631d35ea9cc3c8e6404b5e324aebcbe5575
j3ck <fox_raiodin@mail.ru>
2014-02-15 14:12:10 +0300

    scaffold project

---
+++ db/migrate/20140215111004_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+
+      t.timestamps
+    end
+
+    add_index :projects, :name,                :unique => true
+  end
+end
