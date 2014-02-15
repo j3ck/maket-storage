29a7f2fc3f7e71e9678a181430e8b152d5da4feb
j3ck <fox_raiodin@mail.ru>
2014-02-15 18:05:29 +0300

    create function added

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,2 +1,18 @@
 class VersionsController < ApplicationController
+	def create
+		@version = Version.new(version_params)
+
+		respond_to do |format|
+			if @version.save
+				format.html { redirect_to @version.project, notice: "Version created successfull" }
+			else
+				format.html { render 'versions/add_version', :version => @version }
+			end
+		end
+	end
+
+	private
+		def version_params
+			params.require(:version).permit(:name, :project_id) 
+		end
 end
