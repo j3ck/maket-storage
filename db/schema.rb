e59465fff2e581dedb0e8d99d685fcba17c80529
j3ck <fox_raiodin@mail.ru>
2014-02-21 02:10:13 +0300

    Add role to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217183809) do
+ActiveRecord::Schema.define(version: 20140220230739) do
 
   create_table "makets", force: true do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140217183809) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
