5409bbed98ac1b5e6cd0ebbc76a439dac2c65af2
j3ck <fox_raiodin@mail.ru>
2014-02-21 10:49:20 +0300

    addeed users with cancan

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+	load_and_authorize_resource
+	before_action :set_user, only: [:edit, :update, :destroy]
+
+	def index
+		@users = User.all
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			redirect_to users_url
+		else
+			render action: 'edit'
+		end
+	end
+
+	def destroy
+		@user.destroy
+		redirect_to users_url
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		
+		if @user.save
+			redirect_to users_url
+		else
+			render action: 'new'
+		end
+	end
+
+	private
+		def set_user
+			@user = User.find(params[:id])
+		end
+
+		def user_params
+			params.require(:user).permit(:email, :role, :password, :password_confirmation) 
+		end
+end
