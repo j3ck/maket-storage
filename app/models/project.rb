376a24ef9370d6b318a1d41b66776fbf326747c9
j3ck <fox_raiodin@mail.ru>
2014-02-27 22:51:34 +0300

    prefixes validates error messages

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
 	belongs_to :user
 	has_many :versions, dependent: :destroy
 
-	validates :name, presence: true
-	validates :name, length: { minimum: 2 }
-	validates :name, uniqueness: true
+	validates :name, presence: { message: "не может быть пустым" }
+	validates :name, length: { minimum: 2, message: "длина должна быть минимум два символа" }
+	validates :name, uniqueness: { message: "такое название уже существует" }
 end
--- app/models/version.rb
+++ app/models/version.rb
@@ -2,6 +2,6 @@ class Version < ActiveRecord::Base
 	belongs_to :project
 	has_many :makets, dependent: :destroy
 
-	validates :name, presence: true
-	validates :name, length: { minimum: 2 }
+	validates :name, presence: { message: "не может быть пустым" }
+	validates :name, length: { minimum: 2, message: "длина должна быть минимум два символа" }
 end
