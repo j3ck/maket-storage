87e8fd71bd76a0fe6ce542e3f94822f9fe139422
j3ck <fox_raiodin@mail.ru>
2014-07-05 11:52:07 +0300

    rspec to versions/:show

--- spec/controllers/versions_controller_spec.rb
+++ spec/controllers/versions_controller_spec.rb
@@ -1,9 +1,22 @@
 require "rails_helper.rb"
+require "cancan/matchers"
 
 describe VersionsController do
 
-	before {
-	}
+before {
+	@version = FactoryGirl.create(:version)
+	sign_in :user, @version.project.user
+}
 
+	describe "GET show" do
+		it "get success status" do
+			get :show, { :project_id => @version.project_id, :id => @version.id }
+			expect(response).to be_success
+		end
 
+		it "render show template" do
+			get :show, { :project_id => @version.project_id, :id => @version.id }
+			expect(response).to render_template(:show)
+		end
+	end
 end
